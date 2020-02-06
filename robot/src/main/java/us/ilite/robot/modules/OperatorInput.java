5edc89a4082f4ebdbe474954b13f88ebf8e85fff
Kate <BalintKE20@pwcs-edu.org>
2020-02-06 16:45:33 -0500

    started work on shuffleboard layout

--- robot/src/main/java/us/ilite/robot/modules/OperatorInput.java
+++ robot/src/main/java/us/ilite/robot/modules/OperatorInput.java
@@ -3,10 +3,17 @@ package us.ilite.robot.modules;
 import com.flybotix.hfr.codex.Codex;
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import us.ilite.common.types.EMatchMode;
 import us.ilite.common.types.input.ELogitech310;
+import us.ilite.robot.DriverInput;
 import us.ilite.robot.Robot;
+
+import java.util.TreeMap;
 
 public class OperatorInput extends Module {
     protected static final double
@@ -16,6 +23,9 @@ public class OperatorInput extends Module {
 
     private Joystick mDriverJoystick;
     private Joystick mOperatorJoystick;
+    private ShuffleboardTab drivetrainTab;
+    private NetworkTableEntry driverThrottleEntry;
+    private TreeMap<String, NetworkTableEntry> entryTreeMap;
 
 
 
@@ -24,6 +34,12 @@ public class OperatorInput extends Module {
     public OperatorInput() {
         mDriverJoystick = new Joystick(0);
         mOperatorJoystick = new Joystick(1);
+        drivetrainTab = Shuffleboard.getTab("drivetrain");
+        driverThrottleEntry = drivetrainTab.add("Driver Throttle", 0).getEntry();
+
+        for (int i = 0; i < Robot.DATA.driverinput.length(); i++) {
+            //put strings and corresponding network table entries in the map
+        }
     }
 
     @Override
@@ -39,6 +55,8 @@ public class OperatorInput extends Module {
 
     @Override
     public void setOutputs(double pNow) {
+        System.out.println("-----------------" + Robot.DATA.driverinput.get(ELogitech310.LEFT_Y_AXIS));
+        driverThrottleEntry.setDouble(Robot.DATA.driverinput.get(ELogitech310.LEFT_Y_AXIS));
     }
 
 
