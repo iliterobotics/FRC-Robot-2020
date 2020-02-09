4276bbd7f40e74c70655bc28c5633707e7db4374
Kate <BalintKE20@pwcs-edu.org>
2020-02-09 12:33:25 -0500

    adding more widgets to shuffleboard layout

--- robot/src/main/java/us/ilite/robot/modules/OperatorInput.java
+++ robot/src/main/java/us/ilite/robot/modules/OperatorInput.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import us.ilite.common.types.ELimelightData;
 import us.ilite.common.types.EMatchMode;
+import us.ilite.common.types.EShooterSystemData;
 import us.ilite.common.types.input.ELogitech310;
 import us.ilite.robot.Robot;
 import java.util.List;
@@ -26,13 +27,8 @@ public class OperatorInput extends Module {
     private Joystick mDriverJoystick;
     private Joystick mOperatorJoystick;
     private ShuffleboardTab drivetrainTab;
-//    private NetworkTableEntry operatorThrottleEntry, aButtonEntry, bButtonEntry, xButtonEntry,
-//            yButtonEntry, lButtonEntry, rButtonEntry, backEntry, startEntry, rightJoystickButtonEntry,
-//            leftJoystickButtonEntry, dpadRawEntry, dpadUpEntry, dpadDownEntry, dpadLeftEntry, dpadRightEntry,
-//            leftXAxisEntry, leftYAxisEntry, rightXAxisEntry, rightYAxisEntry, combinedTriggerAxisEntry,
-//            leftTriggerAxisEntry, rightTriggerAxisEntry, rumbleEntry;
     private NetworkTableEntry matchTimeEntry, driverThrottleEntry, limelightStatusEntry, driverHangBtnEntry,
-        operatorHangBtnEntry, intakeStatusEntry;
+        operatorHangBtnEntry, intakeOutEntry, intakeInEntry, intakeRollersCurrentEntry, longShortMode;
     protected Codex<Double, ELogitech310> mDriverInputCodex, mOperatorInputCodex;
     private EMatchMode mMode = EMatchMode.DISABLED;
 
@@ -41,37 +37,16 @@ public class OperatorInput extends Module {
         mDriverJoystick = new Joystick(0);
         mOperatorJoystick = new Joystick(1);
         drivetrainTab = Shuffleboard.getTab("drivetrain");
-//        operatorThrottleEntry = drivetrainTab.add("Operator Throttle", 0).getEntry();
-//        aButtonEntry = drivetrainTab.add("A Button", 0).getEntry();
-//        bButtonEntry = drivetrainTab.add("B Button", 0).getEntry();
-//        xButtonEntry = drivetrainTab.add("X Button", 0).getEntry();
-//        yButtonEntry = drivetrainTab.add("Y Button", 0).getEntry();
-//        lButtonEntry = drivetrainTab.add("L Button", 0).getEntry();
-//        rButtonEntry = drivetrainTab.add("R Button", 0).getEntry();
-//        backEntry = drivetrainTab.add("Back", 0).getEntry();
-//        startEntry = drivetrainTab.add("Start", 0).getEntry();
-//        rightJoystickButtonEntry = drivetrainTab.add("Right Joystick Button", 0).getEntry();
-//        leftJoystickButtonEntry = drivetrainTab.add("Left Joystick Button", 0).getEntry();
-//        dpadRawEntry = drivetrainTab.add("DPad Raw", 0).getEntry();
-//        dpadUpEntry = drivetrainTab.add("DPad Up", 0).getEntry();
-//        dpadDownEntry = drivetrainTab.add("DPad Down", 0).getEntry();
-//        dpadLeftEntry = drivetrainTab.add("DPad Left", 0).getEntry();
-//        dpadRightEntry = drivetrainTab.add("DPad Right", 0).getEntry();
-//        leftXAxisEntry = drivetrainTab.add("Left X Axis", 0).getEntry();
-//        leftYAxisEntry = drivetrainTab.add("Left Y Axis", 0).getEntry();
-//        rightXAxisEntry = drivetrainTab.add("Right X Axis", 0).getEntry();
-//        rightYAxisEntry = drivetrainTab.add("Right Y Axis", 0).getEntry();
-//        combinedTriggerAxisEntry = drivetrainTab.add("Combined Trigger Axis", 0).getEntry();
-//        leftTriggerAxisEntry = drivetrainTab.add("Left Trigger Axis", 0).getEntry();
-//        rightTriggerAxisEntry = drivetrainTab.add("Right Trigger Axis", 0).getEntry();
-//        rumbleEntry = drivetrainTab.add("Rumble", 0).getEntry();
 
         matchTimeEntry = drivetrainTab.add("Match Time", 0).withSize(2, 1).withPosition(0, 0).getEntry();
         driverThrottleEntry = drivetrainTab.add("Driver Throttle", 0).withWidget(BuiltInWidgets.kGraph).getEntry();
         limelightStatusEntry = drivetrainTab.add("Is Target Locked", 0).getEntry();
         driverHangBtnEntry = drivetrainTab.add("Hanger Lock - Driver", 0).getEntry();
         operatorHangBtnEntry = drivetrainTab.add("Hanger Lock - Operator", 0).getEntry();
-        intakeStatusEntry = drivetrainTab.add("Intake Status", 0).getEntry();
+        intakeOutEntry = drivetrainTab.add("Intake Out", 0).getEntry();
+        intakeInEntry = drivetrainTab.add("Intake Out", 0).getEntry();
+        intakeRollersCurrentEntry = drivetrainTab.add("Intake Current", 0).getEntry();
+        longShortMode = drivetrainTab.add("Mode - Long/Short", " ").getEntry();
     }
 
     @Override
@@ -94,34 +69,11 @@ public class OperatorInput extends Module {
 
     @Override
     public void setOutputs(double pNow) {
-        driverThrottleEntry.setDouble(Robot.DATA.driverinput.get(ELogitech310.LEFT_Y_AXIS));
-//        operatorThrottleEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.LEFT_Y_AXIS));
-//        aButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.A_BTN));
-//        bButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.B_BTN));
-//        xButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.X_BTN));
-//        yButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.Y_BTN));
-//        lButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.L_BTN));
-//        rButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.R_BTN));
-//        backEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.BACK));
-//        startEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.A_BTN));
-//        rightJoystickButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.RIGHT_JOYSTICK_BTN));
-//        leftJoystickButtonEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.LEFT_JOYSTICK_BTN));
-//        dpadRawEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.DPAD_RAW));
-//        dpadUpEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.DPAD_UP));
-//        dpadDownEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.DPAD_DOWN));
-//        dpadLeftEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.DPAD_LEFT));
-//        dpadRightEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.DPAD_RIGHT));
-//        leftXAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.LEFT_X_AXIS));
-//        leftYAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.LEFT_Y_AXIS));
-//        rightXAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.RIGHT_X_AXIS));
-//        rightYAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.RIGHT_Y_AXIS));
-//        combinedTriggerAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.COMBINED_TRIGGER_AXIS));
-//        leftTriggerAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.LEFT_TRIGGER_AXIS));
-//        rightTriggerAxisEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.RIGHT_TRIGGER_AXIS));
-//        rumbleEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.RUMBLE));
-
         //display current match time
         matchTimeEntry.setDouble(Robot.CLOCK.getCurrentTime());
+
+        //display driver throttle
+        driverThrottleEntry.setDouble(Robot.DATA.driverinput.get(ELogitech310.LEFT_Y_AXIS));
 
         //display if limelight sees target and/or is locked
         if(Robot.DATA.limelight.get(ELimelightData.TV) == 1)
@@ -141,13 +93,22 @@ public class OperatorInput extends Module {
         operatorHangBtnEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.Y_BTN));
         driverHangBtnEntry.setDouble(Robot.DATA.driverinput.get(ELogitech310.BACK));
 
-        //long/short mode (hood angle and flywheel speed)
+        //long/short shooting mode
+        if(Robot.DATA.flywheel.get(EShooterSystemData.CURRENT_FLYWHEEL_VELOCITY) > 0.0) //arbitrary value for now, need clarification
+        {
+          longShortMode.setString("Long");
+        }
+        else
+        {
+          longShortMode.setString("Short");
+        }
 
         //intake in or out and current to tell what the operator's trying to do
-        //intakeStatusEntry.setDouble(Robot.DATA.operatorinput.get())
+        intakeOutEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.X_BTN));
+        intakeInEntry.setDouble(Robot.DATA.operatorinput.get(ELogitech310.A_BTN));
 
-        //ball count
-
+        //ball count - check how many times the secondary beam is broken
+        if(Robot.DATA.powercell.get()
 
         if (mMode == EMatchMode.TEST) {
             for(RobotCodex c : Robot.DATA.mLoggedCodexes) {
