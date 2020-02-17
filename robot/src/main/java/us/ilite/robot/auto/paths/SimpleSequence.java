bfa21e0a5e6fb699b1d926ca626fb3a3da8f751b
John Anselmo <jjohnanselmo@gmail.com>
2020-02-17 18:15:59 -0500

    Testing backwards pathing - error on constantly positive velocities

--- robot/src/main/java/us/ilite/robot/auto/paths/SimpleSequence.java
+++ robot/src/main/java/us/ilite/robot/auto/paths/SimpleSequence.java
@@ -12,7 +12,7 @@ public class SimpleSequence implements ISequence {
     private static int beamCounter = 0;
     private static boolean mLastSecondaryBeamBroken = false;
     private static boolean mSecondaryBeamBroken = false;
-    private static IAutoCommand[] mSteps = {new IntakeCommand(2)};
+    private static IAutoCommand[] mSteps = {new IntakeCommand(15)};
     private static int mCurrentCommandIndex = 0;
     private double mCurrentDistance = 0;
 
