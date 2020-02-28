3514c5ccd9167f1f1f4a4d373deb9324ee23a76d
John Anselmo <jjohnanselmo@gmail.com>
2020-02-27 21:13:18 -0500

    added flywheel capabilities

--- robot/src/main/java/us/ilite/robot/controller/YoinkController.java
+++ robot/src/main/java/us/ilite/robot/controller/YoinkController.java
@@ -3,6 +3,10 @@ package us.ilite.robot.controller;
 import com.team2363.commands.IliteHelixFollower;
 import com.team319.trajectory.Path;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import us.ilite.common.Angle;
+import us.ilite.common.Distance;
+import us.ilite.common.Field2020;
+import us.ilite.common.types.ELimelightData;
 import us.ilite.common.types.EPowerCellData;
 import us.ilite.common.types.EShooterSystemData;
 import us.ilite.common.types.drive.EDriveData;
@@ -37,8 +41,28 @@ public class YoinkController extends BaseAutonController {
                 super.updateImpl(pNow);
             }
             if (BobUtils.isFinished(pNow, mYoinkFrom, mYoinkFromStartTime)) {
+                Enums.FlywheelSpeeds flywheelState = Enums.FlywheelSpeeds.CLOSE;
                 setTargetTracking(true);
+                if (Field2020.canHitInnerGoal(tempCalcAngleToInnerGoal(), Distance.fromFeet(db.goaltracking.get(ELimelightData.CALC_DIST_TO_TARGET)))) {
+                    setFlywheelClosedLoop(flywheelState);
+                    if (isFlywheelUpToSpeed()) {
+                        setFeederClosedLoop(flywheelState);
+                        if (isFeederUpToSpeed()) {
+                            db.powercell.set(EPowerCellData.SET_V_pct, 0.6);
+                            db.powercell.set(EPowerCellData.SET_H_pct, 0.5);
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private Angle tempCalcAngleToInnerGoal() {
+        double thetaGoal = db.limelight.get(ELimelightData.CALC_ANGLE_TO_TARGET);
+        double distToGoal = db.limelight.get(ELimelightData.CALC_DIST_TO_TARGET); //TODO: Units??
+        double b = 29.25 + (distToGoal * Math.cos(thetaGoal));
+        double a = distToGoal * Math.sin(thetaGoal);
+        double thetab = Math.tanh(b/a);
+        return Angle.fromDegrees(90 - thetab);
     }
 }
